01b7f690757386e637ccdb17c4b801efd112d57d
Skyler Escobedo <skylerescobedo@gmail.com>
2018-06-13 20:09:45 -0700

    Lifted up graphql query to find current team and channel

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,21 +1,48 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import { findIndex } from "lodash";
 import Header from "../components/Header";
 import Messages from "../components/Messages";
 import AppLayout from "../components/AppLayout";
 import SendMessage from "../components/SendMessage";
 import Sidebar from "../containers/Sidebar";
+import { allTeamsQuery } from "../graphql/team";
 
-const ViewTeam = ({ match: { params } }) => (
-  <AppLayout>
-    <Sidebar currentTeamId={params.teamId}/>
-    <Header channelName="general" />
-    <Messages>
-      <ul className="message-list">
-        <li />
-        <li />
-      </ul>
-    </Messages>
-    <SendMessage channelName="general" />
-  </AppLayout>
-);
-export default ViewTeam;
+const ViewTeam = ({
+  data: { loading, allTeams },
+  match: {
+    params: { teamId, channelId }
+  }
+}) => {
+  if (loading) {
+    return null;
+  }
+  // Checking for a valid currentTeamId,
+  //   if true - look up that currentTeamId in allTeams,
+  //   if false - get the element at the zeroth index.
+  const teamIdx = teamId ? findIndex(allTeams, ["id", parseInt(teamId, 10)]) : 0;
+  const team = allTeams[teamIdx];
+  const channelIdx = channelId ? findIndex(team.channels, ["id", parseInt(channelId, 10)]) : 0;
+  const channel = team.channels[channelIdx];
+  
+  return (
+    <AppLayout>
+      <Sidebar
+        teams={allTeams.map(t => ({
+          id: t.id,
+          letter: t.name.charAt(0).toUpperCase()
+        }))}
+        team={team}
+      />
+      <Header channelName={channel.name}/>
+      <Messages channelId={channel.id}>
+        <ul className="message-list">
+          <li />
+          <li />
+        </ul>
+      </Messages>
+      <SendMessage channelName={channel.name} />
+    </AppLayout>
+  );
+};
+export default graphql(allTeamsQuery)(ViewTeam);
